10abecdbc8c728057c872c4e94ba6f1886fdaadd
Preet Pambhar <preetpambhar73530@gmail.com>
2025-01-21 02:49:39 -0500

    feat: tabView

--- BrewIQ/ContentView.swift
+++ BrewIQ/ContentView.swift
@@ -9,54 +9,25 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
+    @State private var activeTab: Tab = .home
+    
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .navigationTitle("Movies")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
+        TabView(selection: $activeTab){
+            Home()
+                .tag(Tab.home)
+                .tabItem { Tab.home.tabContent }
+            
+            CoffeeInfo()
+                .tag(Tab.coffeeInfo)
+                .tabItem { Tab.coffeeInfo.tabContent }
+            
+            UserProfile()
+                .tag(Tab.userProfile)
+                .tabItem { Tab.userProfile.tabContent }
         }
     }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
