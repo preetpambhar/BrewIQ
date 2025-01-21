495b4d7ded3dbe15fd34fa3b9f9d136d60024269
Preet Pambhar <preetpambhar73530@gmail.com>
2025-01-21 02:47:07 -0500

    feat: CoffeeInfo

--- BrewIQ/View/CoffeeInfo/View/CoffeeCard.swift
+++ BrewIQ/View/CoffeeInfo/View/CoffeeCard.swift
@@ -9,8 +9,31 @@ import SwiftUI
 
 struct CoffeeCard: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading){
+            Image("coffee")
+                .resizable()
+                .frame(width: 150, height: 150)
+                .scaledToFit()
+                .border(Color.gray, width: 2)
+                .shadow(radius: 2)
+                .cornerRadius(15)
+            
+            VStack (alignment: .leading){
+                Text("Coffee Name")
+                    .font(.headline)
+                    .fontWeight(.semibold)
+            }
+            .padding(.top)
+        }
+        .padding(.vertical, 12)
+        .padding(.horizontal)
+//        .border(Color.gray, width: 0.5)
+//        .shadow(color: .gray,radius: 15)
+        .background(Color.white)
+        .cornerRadius(15)
+        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
     }
+        
 }
 
 #Preview {
--- BrewIQ/View/CoffeeInfo/View/CoffeeInfoRowView.swift
+++ BrewIQ/View/CoffeeInfo/View/CoffeeInfoRowView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct CoffeeInfoRowView: View {
+    @State private var searchText = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack{
+                Text("Select a coffee to get more details")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                ScrollView {
+                    ForEach(1...10, id: \.self) { _ in
+                        HStack {
+                            CoffeeCard()
+                            CoffeeCard()
+                        }
+                    }
+                }
+            }
+        }
+        .navigationTitle("Coffees")
+//        .searchable(text: $searchText, prompt:"Search flowers...")
+//        .task {
+//           // print("searching..\($searchText)")
+//           // searchCoffee(result: searchText)
+//        }
+    }
+    
+    func searchCoffee(result: String) -> String{
+        print(result)
+        return result
     }
 }
 
