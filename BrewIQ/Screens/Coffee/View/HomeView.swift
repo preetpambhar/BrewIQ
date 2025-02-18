66099d25811cb8d4218d47d1d7d8fdc35679089b
Preet Pambhar <preetpambhar73530@gmail.com>
2025-02-17 19:53:11 -0500

    fix

--- BrewIQ/Screens/Coffee/View/HomeView.swift
+++ BrewIQ/Screens/Coffee/View/HomeView.swift
@@ -25,107 +25,138 @@ struct HomeView: View {
         
     
     var body: some View {
-        VStack(spacing: 20) {
-                      // Welcome Header
-          
-            VStack {
-                if let currentUser = authViewModel.currentUser{
-                    Text(CoffeeController.getGreeting() + currentUser.userName)
+        ScrollView{
+            VStack(spacing: 15) {
+                // Welcome Header
+                VStack {
+                    HStack{
+                        Image("coffee1")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 60, height: 60)
+                            .cornerRadius(50)
+                        VStack(alignment: .leading){
+                            if let currentUser = authViewModel.currentUser{
+                                Text(currentUser.userName)
+                                    .font(.title2)
+                                    .fontWeight(.semibold)
+                                    .multilineTextAlignment(.center)
+                            } else {
+                                Text("Preet")
+                                    .font(.title2)
+                                    .fontWeight(.semibold)
+                                    .multilineTextAlignment(.center)
+                                Text("Patel")
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                    .multilineTextAlignment(.center)
+                            }
+                        }
+                        Spacer()
+                        
+                    }
+                    if let currentUser = authViewModel.currentUser{
+                        Text(CoffeeController.getGreeting() + currentUser.userName)
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .multilineTextAlignment(.center)
+                    } else {
+                        Text(CoffeeController.getGreeting() + "Ankita Devani")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .multilineTextAlignment(.center)
+                    }
+                    
+                    Text("What’s your coffee mood today?")
                         .font(.title2)
                         .fontWeight(.semibold)
                         .multilineTextAlignment(.center)
+                    
+                } .padding()
+                // Mood Selector
+                VStack(alignment: .leading) {
+                    Text("Select Your Mood:")
+                        .font(.headline)
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 15) {
+                            ForEach(moods, id: \.self) { mood in
+                                Button(action: {
+                                    selectedMood = mood
+                                }) {
+                                    Text(mood)
+                                        .font(.title3)
+                                        .padding()
+                                        .background(selectedMood == mood ? Color.blue.opacity(0.2) : Color.gray.opacity(0.1))
+                                        .cornerRadius(10)
+                                }
+                            }
+                        }
+                    }
                 }
+                .padding(.horizontal)
                 
-                Text("What’s your coffee mood today?")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .multilineTextAlignment(.center)
-                  
-            } .padding()
-                      // Mood Selector
-                      VStack(alignment: .leading) {
-                          Text("Select Your Mood:")
-                              .font(.headline)
-                          ScrollView(.horizontal, showsIndicators: false) {
-                              HStack(spacing: 15) {
-                                  ForEach(moods, id: \.self) { mood in
-                                      Button(action: {
-                                          selectedMood = mood
-                                      }) {
-                                          Text(mood)
-                                              .font(.title3)
-                                              .padding()
-                                              .background(selectedMood == mood ? Color.blue.opacity(0.2) : Color.gray.opacity(0.1))
-                                              .cornerRadius(10)
-                                      }
-                                  }
-                              }
-                          }
-                      }
-                      .padding(.horizontal)
-                      
-                      // Caffeine Level Slider
-                      VStack(alignment: .leading) {
-                          Text("Select Caffeine Level:")
-                              .font(.headline)
-                          Slider(value: $caffeineLevel, in: 0...100, step: 10)
-                              .accentColor(.blue)
-                          Text("Caffeine: \(Int(caffeineLevel))%")
-                              .font(.subheadline)
-                              .foregroundColor(.gray)
-                      }
-                      .padding(.horizontal)
-                      
-                      // Cup Size Picker
-                      VStack(alignment: .leading) {
-                          Text("Choose Cup Size:")
-                              .font(.headline)
-                          Picker("Cup Size", selection: $cupSize) {
-                              ForEach(cupSizes, id: \.self) { size in
-                                  Text(size)
-                              }
-                          }
-                          .pickerStyle(SegmentedPickerStyle())
-                      }
-                      .padding(.horizontal)
-                      
-                      // Suggestion Button
-                      Button(action: {
-                         
-                          // Call SuggestCoffee method and update the suggestion
-                            coffeeSuggestion = coffeeController.SuggestCoffee(
-                                selectedMood: selectedMood,
-                                              caffeineLevel: caffeineLevel,
-                                              cupSize: cupSize,
-                                timeOfDay: CoffeeController.getTimeOfDay(), activityLevel: "Sedentary", isCalorieConscious: true
-                                          )
-                      }) {
-                          Text("Find My Brew")
-                              .font(.headline)
-                              .foregroundColor(.white)
-                              .padding()
-                              .frame(maxWidth: .infinity)
-                              .background(Color.blue)
-                              .cornerRadius(10)
-                      }
-                      .padding(.horizontal)
-                      
-            
-                      if !coffeeSuggestion.isEmpty {
-                           Text("Suggested Coffee: \(coffeeSuggestion)")
-                               .font(.headline)
-                               .padding()
-                       }
-                      Spacer()
-            
-            if authViewModel.currentUser != nil{
-                Button ("Sign Out"){
-                   authViewModel.signOut()
+                // Caffeine Level Slider
+                VStack(alignment: .leading) {
+                    Text("Select Caffeine Level:")
+                        .font(.headline)
+                    Slider(value: $caffeineLevel, in: 0...100, step: 10)
+                        .accentColor(.blue)
+                    Text("Caffeine: \(Int(caffeineLevel))%")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
                 }
+                .padding(.horizontal)
+                
+                // Cup Size Picker
+                VStack(alignment: .center) {
+                    Text("Choose Cup Size:")
+                        .font(.headline)
+                    Picker("Cup Size", selection: $cupSize) {
+                        ForEach(cupSizes, id: \.self) { size in
+                            Text(size)
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    Image("coffee1")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 120, height: 120)
+                        .cornerRadius(10)
+                }
+                .padding(.horizontal)
+                
+                // Suggestion Button
+                Button(action: {
+                    
+                    // Call SuggestCoffee method and update the suggestion
+                    coffeeSuggestion = coffeeController.SuggestCoffee(
+                        selectedMood: selectedMood,
+                        caffeineLevel: caffeineLevel,
+                        cupSize: cupSize,
+                        timeOfDay: CoffeeController.getTimeOfDay(), activityLevel: "Sedentary", isCalorieConscious: true
+                    )
+                }) {
+                    Text("Find My Brew")
+                }
+                .buttonStyle(CapsuleButtonStyle())
+                .padding(.horizontal)
+                
+                
+                if !coffeeSuggestion.isEmpty {
+                    Text("Suggested Coffee: \(coffeeSuggestion)")
+                        .font(.headline)
+                        .padding()
+                }
+                Spacer()
+                
+                //            if authViewModel.currentUser != nil{
+                //                Button ("Sign Out"){
+                //                   authViewModel.signOut()
+                //                }
+                //            }
             }
+            .navigationTitle("BrewIQ")
         }
-                  .navigationTitle("BrewIQ")
-                  
     }
 }
 
